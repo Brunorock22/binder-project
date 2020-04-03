fe8fef93bbeff55d2670c6b9d18a9e9b451b4a8a
Bruno Camargos <brunocamargos2009@hotmail.com>
2020-04-02 21:10:39 -0300

    Updates

--- lib/componentes/dialog_custom.dart
+++ lib/componentes/dialog_custom.dart
@@ -1,14 +1,18 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/widgets.dart';
 
-class DialogCustom{
-static void showCustomDialog(BuildContext context, String title, String message, DialogType type) {
-AwesomeDialog(context: context,
-dialogType: type,
-animType: AnimType.BOTTOMSLIDE,
-tittle: title,
-desc: message,
+class DialogCustom {
+  static void showCustomDialog(
+      BuildContext context, String title, String message, DialogType type) {
+    AwesomeDialog(
+        context: context,
+        dialogType: type,
+        animType: AnimType.BOTTOMSLIDE,
+        tittle: title,
+        desc: message,
 //btnCancelOnPress: () {},
-btnOkOnPress: () {}).show();
-}
+        btnOkOnPress: () {
+          Navigator.of(context, rootNavigator: true).pop();
+        }).show();
+  }
 }
--- lib/screens/formulario_upload.dart
+++ lib/screens/formulario_upload.dart
@@ -3,6 +3,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:trabalho_sistemas/componentes/dialog_custom.dart';
+import 'package:trabalho_sistemas/componentes/flushbar_custom.dart';
 import 'package:trabalho_sistemas/database/dao/materia_dao.dart';
 import 'package:trabalho_sistemas/model/materias.dart';
 import 'package:trabalho_sistemas/util/colors_util.dart';
@@ -22,36 +23,34 @@ class _FormularioState extends State<Formulario> {
   var isPdfPicked = false;
   var isImagePicked = false;
 
-
   @override
   void initState() {
     super.initState();
-    if(widget.materia.anotacoes != null){
+    if (widget.materia.anotacoes != null) {
       anotacaotField.text = widget.materia.anotacoes;
     }
-    if(widget.materia.dataEscolhida != null) {
+    if (widget.materia.dataEscolhida != null) {
       DateTime myDate = DateTime.parse(widget.materia.dataEscolhida);
       dataTextField.text = "${myDate.day}/${myDate.month}/${myDate.year}";
     }
-    if(widget.materia.pathImg != null){
+    if (widget.materia.pathImg != null) {
       isImagePicked = true;
     }
-    if(widget.materia.pathPdf != null){
+    if (widget.materia.pathPdf != null) {
       isPdfPicked = true;
     }
     anotacaotField.addListener(() {
-    widget.materia.anotacoes = anotacaotField.text;
+      widget.materia.anotacoes = anotacaotField.text;
     });
   }
 
   @override
   Widget build(BuildContext mContext) {
-
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.white),
-          backgroundColor: ColorUtils.accentColor ,
+          backgroundColor: ColorUtils.accentColor,
         ),
         body: SingleChildScrollView(
           child: Padding(
@@ -63,7 +62,9 @@ class _FormularioState extends State<Formulario> {
                   child: TextFormField(
                     controller: dataTextField,
                     readOnly: true,
-                    style: TextStyle( color: ColorUtils.accentColor,),
+                    style: TextStyle(
+                      color: ColorUtils.accentColor,
+                    ),
                     decoration: InputDecoration(
                       suffixIcon: Icon(
                         Icons.calendar_today,
@@ -85,7 +86,9 @@ class _FormularioState extends State<Formulario> {
                   padding: const EdgeInsets.only(bottom: 12.0),
                   child: TextFormField(
                     controller: anotacaotField,
-                    style: TextStyle( color: ColorUtils.accentColor,),
+                    style: TextStyle(
+                      color: ColorUtils.accentColor,
+                    ),
                     decoration: InputDecoration(
                       suffixIcon: Icon(
                         Icons.text_fields,
@@ -114,9 +117,13 @@ class _FormularioState extends State<Formulario> {
                             border: Border.all(
                                 width: 2.0, color: ColorUtils.primaryColor)),
                         child: Icon(
-                          isImagePicked?Icons.done_outline:Icons.photo_camera,
+                          isImagePicked
+                              ? Icons.done_outline
+                              : Icons.photo_camera,
                           size: 40.0,
-                          color: isImagePicked?Colors.green:ColorUtils.accentColor,
+                          color: isImagePicked
+                              ? Colors.green
+                              : ColorUtils.accentColor,
                         )),
                     onTap: () => openFile(FileType.image),
                   ),
@@ -133,9 +140,13 @@ class _FormularioState extends State<Formulario> {
                             border: Border.all(
                                 width: 2.0, color: ColorUtils.primaryColor)),
                         child: Icon(
-                          isPdfPicked?Icons.done_outline:Icons.picture_as_pdf,
+                          isPdfPicked
+                              ? Icons.done_outline
+                              : Icons.picture_as_pdf,
                           size: 40.0,
-                          color:  isPdfPicked?Colors.green:ColorUtils.accentColor,
+                          color: isPdfPicked
+                              ? Colors.green
+                              : ColorUtils.accentColor,
                         )),
                     onTap: () => openFile(FileType.custom),
                   ),
@@ -146,25 +157,24 @@ class _FormularioState extends State<Formulario> {
                         widget.materia.pathImg == null ||
                         widget.materia.anotacoes == null ||
                         widget.materia.dataEscolhida == null) {
-//                      FlusBarCustom(
-//                          "Preencha todos os campos.",
-//                          context,
-//                          Icon(
-//                            Icons.error,
-//                            color: Colors.orangeAccent,
-//                          )).flushbar();
+                      FlusBarCustom(
+                          "Preencha todos os campos.",
+                          mContext,
+                          Icon(
+                            Icons.error,
+                            color: Colors.orangeAccent,
+                          )).flushbar();
                     } else {
-                      saveDocuments(context);
+                      saveDocuments(mContext);
                     }
                   },
-
                   child: Container(
                     height: 50,
                     width: 100,
                     child: Row(
                       children: <Widget>[
                         Padding(
-                          padding: const EdgeInsets.only(left:12.0),
+                          padding: const EdgeInsets.only(left: 12.0),
                           child: Icon(
                             Icons.save,
                             color: Colors.white,
@@ -230,46 +240,10 @@ class _FormularioState extends State<Formulario> {
 
   Future<void> saveDocuments(BuildContext mContext) async {
     MateriaDao dao = MateriaDao();
-    FutureBuilder(
-        future: Future.delayed(Duration(seconds: 1))
-            .then((value) => dao.update(widget.materia)),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              break;
-            case ConnectionState.waiting:
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SpinKitSquareCircle(
-                      color: ColorUtils.primaryColor,
-                      size: 50.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 35.0),
-                      child: Text(
-                        'Carregando...',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 14,
-                            color: Colors.black54),
-                      ),
-                    )
-                  ],
-                ),
-              );
-              break;
-            case ConnectionState.active:
-              break;
-            case ConnectionState.done:
-              DialogCustom.showCustomDialog(mContext, "Salvo com Sucesso",
-                  "Dados foram salvos no lembrete", DialogType.SUCCES);
-              break;
-          }
-          return null;
-        });
+    dao.update(widget.materia).then((value) {
+      DialogCustom.showCustomDialog(mContext, "Salvo com Sucesso",
+          "Dados foram salvos no lembrete", DialogType.SUCCES);
+    });
   }
 
   void datePicker(BuildContext mContext) async {
